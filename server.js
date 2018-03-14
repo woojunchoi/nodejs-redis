d3a947dffa7d3bae3377461a8cbb27ac0a229744
woojunchoi <tim.woojune1@gmail.com>
2018-03-14 00:07:05 -0700

    added handlebars and overrid

--- server.js
+++ server.js
@@ -4,3 +4,20 @@ const path = require('path');
 const bodyParser = require('body-parser');
 const methodOverride = require('method-override');
 const redis = require('redis');
+
+//set port
+const port = 3000
+
+//Init app
+const app = express()
+
+//view engine
+app.engine('handlebars', exphbs({defaultLayout:'main'}));
+app.set('view engine', 'handlebars');
+
+// body-parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
+
+// methodOverride
+app.use(methodOverride('_method'));
